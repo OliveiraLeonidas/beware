6d3951f1a8592b9f265afc380f1332b40102f1c5
OliveiraLeonidas <leonidasoliv25@gmail.com>
2025-08-10 01:31:12 -0300

    feat: ensure that a product can be removed from cart

---
+++ src/actions/remove-cart-product/schema.ts
@@ -0,0 +1,8 @@
+import z from "zod";
+
+
+export const removeProductFromCartSchema = z.object({
+  cartItemId: z.uuid(),
+})
+
+export type removeProductFromCartSchema = z.infer<typeof removeProductFromCartSchema>
